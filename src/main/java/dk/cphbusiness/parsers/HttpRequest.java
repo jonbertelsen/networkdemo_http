6c474d8f8ceaa4707c450544c18b11cdc4ba8283
Jon Bertelsen <jobe@cphbusiness.dk>
2024-02-09 01:52:46 +0100

    add query and form params to parser

--- src/main/java/dk/cphbusiness/parsers/HttpRequest.java
+++ src/main/java/dk/cphbusiness/parsers/HttpRequest.java
@@ -8,8 +8,11 @@ public class HttpRequest
     private String httpRequest;
     private String httpMethod;
     private String url;
+    private String urlPath;
     private String httpVersion;
-    private Map<String, String> headerMap = new HashMap<>();
+    private Map<String, String> headers = new HashMap<>();
+    private Map<String, String> formParams = new HashMap<>();
+    private Map<String, String> queryParams = new HashMap<>();
 
     public HttpRequest(String httpRequest)
     {
@@ -24,18 +27,24 @@ public class HttpRequest
         {
             parseRequestLine(lines[0]);
             parseHeaderLines(lines);
+            if (httpMethod.toUpperCase().equals("POST") || httpMethod.toUpperCase().equals("PUT"))
+            {
+                if (getHeaderValueByKey("Content-Type").equals("application/x-www-form-urlencoded"))
+                {
+                    parseRequestBodyAsFormData(lines);
+                }
+            }
         }
-
     }
 
     private void parseHeaderLines(String[] lines)
     {
-        for (int i = 1; i < lines.length; i++)
+        for (int i = 1; i < lines.length && !lines[i].isEmpty(); i++)
         {
             String[] headerLine = lines[i].split(":", 2);
             if (headerLine.length == 2)
             {
-                headerMap.put(headerLine[0].trim(), headerLine[1].trim());
+                headers.put(headerLine[0].trim(), headerLine[1].trim());
             }
         }
     }
@@ -48,7 +57,54 @@ public class HttpRequest
             httpMethod = requestLine[0];
             url = requestLine[1];
             httpVersion = requestLine[2];
+
+            if (url.contains("?"))
+            {
+                String[] urlParts = url.split("\\?");
+                urlPath = urlParts[0];
+                String[] queryParamPairs = urlParts[1].split("&");
+                for (int i = 0; i < queryParamPairs.length; i++)
+                {
+                    String[] queryPair = queryParamPairs[i].split("=");
+                    queryParams.put(queryPair[0], queryPair[1]);
+                }
+            } else
+            {
+                urlPath = url;
+            }
         }
+    }
+
+    private void parseRequestBodyAsFormData(String[] lines)
+    {
+        for (int i = 0; i < lines.length; i++)
+        {
+            if (lines[i].isEmpty())
+            {
+                parseFormParameters(lines[i + 1]);
+            }
+        }
+    }
+
+    private void parseFormParameters(String line)
+    {
+        String[] parameterPairs = line.split("&");
+        if (parameterPairs.length > 0)
+        {
+            for (int i = 0; i < parameterPairs.length; i++)
+            {
+                String[] param = parameterPairs[i].split("=");
+                if (param.length == 2)
+                {
+                    formParams.put(param[0], param[1]);
+                }
+            }
+        }
+    }
+
+    public String getHttpRequest()
+    {
+        return httpRequest;
     }
 
     public String getHttpMethod()
@@ -66,13 +122,43 @@ public class HttpRequest
         return httpVersion;
     }
 
-    public Map<String, String> getHeaderMap()
+    public Map<String, String> getHeaders()
     {
-        return headerMap;
+        return headers;
     }
 
     public String getHost()
     {
-        return headerMap.getOrDefault("Host", "");
+        return headers.getOrDefault("Host", "");
+    }
+
+    public Map<String, String> getFormParams()
+    {
+        return formParams;
+    }
+
+    public String getHeaderValueByKey(String key)
+    {
+        return headers.getOrDefault(key, "");
+    }
+
+    public String getBodyParam(String key)
+    {
+        return formParams.getOrDefault(key, "");
+    }
+
+    public String getUrlPath()
+    {
+        return urlPath;
+    }
+
+    public Map<String, String> getQueryParams()
+    {
+        return queryParams;
+    }
+
+    public String getQueryParam(String key)
+    {
+        return queryParams.getOrDefault(key, "");
     }
 }
--- src/test/java/dk/cphbusiness/parsers/HttpRequestTest.java
+++ src/test/java/dk/cphbusiness/parsers/HttpRequestTest.java
@@ -1,22 +1,23 @@
 package dk.cphbusiness.parsers;
 
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class HttpRequestTest
 {
-    static String httpRequestString;
-    static HttpRequest httpRequest;
+    static String httpGetRequestString;
+    static HttpRequest httpGetRequest;
+    static String httpPostRequestString;
+    static HttpRequest httpPostRequest;
 
     @BeforeAll
     static void initialize()
     {
-        httpRequestString = "GET /pages/index.html HTTP/1.1" + System.lineSeparator() +
+        httpGetRequestString = "GET /pages/index.html?id=123&category=cars&year=2017 HTTP/1.1" + System.lineSeparator() +
                 "Host: www.example.com" + System.lineSeparator() +
                 "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3" + System.lineSeparator() +
                 "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" + System.lineSeparator() +
@@ -25,40 +26,100 @@ class HttpRequestTest
                 "Connection: keep-alive" + System.lineSeparator() +
                 "Upgrade-Insecure-Requests: 1";
 
-        httpRequest = new HttpRequest(httpRequestString);
+        httpGetRequest = new HttpRequest(httpGetRequestString);
 
+        httpPostRequestString = "POST /pages/index.html HTTP/1.1" + System.lineSeparator() +
+                "Host: www.example.com" + System.lineSeparator() +
+                "Content-Type: application/x-www-form-urlencoded" + System.lineSeparator() +
+                "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3" + System.lineSeparator() +
+                "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" + System.lineSeparator() +
+                "Accept-Language: en-US,en;q=0.5" + System.lineSeparator() +
+                "Accept-Encoding: gzip, deflate, br" + System.lineSeparator() +
+                "Connection: keep-alive" + System.lineSeparator() +
+                "Upgrade-Insecure-Requests: 1" + System.lineSeparator() +
+                System.lineSeparator() +
+                "login=popeye&password=sailor&role=boss";
+
+        httpPostRequest = new HttpRequest(httpPostRequestString);
     }
 
     @Test
     void getHttpMethod()
     {
-        assertEquals("GET", httpRequest.getHttpMethod());
+        assertEquals("GET", httpGetRequest.getHttpMethod());
     }
 
     @Test
     void getUrl()
     {
-        assertEquals("/pages/index.html", httpRequest.getUrl());
+        assertEquals("/pages/index.html?id=123&category=cars&year=2017", httpGetRequest.getUrl());
+        assertEquals("/pages/index.html", httpPostRequest.getUrl());
     }
 
     @Test
     void getHttpVersion()
     {
-        assertEquals("HTTP/1.1", httpRequest.getHttpVersion());
+        assertEquals("HTTP/1.1", httpGetRequest.getHttpVersion());
     }
 
 
     @Test
     void getHeaderMap()
     {
-        Map<String, String> headerMap = httpRequest.getHeaderMap();
+        Map<String, String> headerMap = httpGetRequest.getHeaders();
         assertEquals(7, headerMap.size());
     }
-
 
     @Test
     void getHost()
     {
-        assertEquals("www.example.com", httpRequest.getHost());
+        assertEquals("www.example.com", httpGetRequest.getHost());
+    }
+
+    @Test
+    void getBodyMap()
+    {
+        Map<String, String> bodyMap = httpPostRequest.getFormParams();
+        assertEquals(3, bodyMap.size());
+    }
+
+    @Test
+    void getBody()
+    {
+        Map<String, String> bodyMap = httpPostRequest.getFormParams();
+        assertEquals("popeye", bodyMap.get("login"));
+        assertEquals("sailor", bodyMap.get("password"));
+        assertEquals("boss", bodyMap.get("role"));
+    }
+
+    @Test
+    void getBodyParam()
+    {
+        assertEquals("popeye", httpPostRequest.getBodyParam("login"));
+        assertEquals("sailor", httpPostRequest.getBodyParam("password"));
+        assertEquals("boss", httpPostRequest.getBodyParam("role"));
+    }
+
+    @Test
+    void getQueryParam()
+    {
+        assertEquals("123", httpGetRequest.getQueryParam("id"));
+        assertEquals("cars", httpGetRequest.getQueryParam("category"));
+        assertEquals("2017", httpGetRequest.getQueryParam("year"));
+        assertEquals("", httpGetRequest.getQueryParam("bennybadebold"));
+    }
+
+    @Test
+    void getUrlPath()
+    {
+        assertEquals("/pages/index.html", httpGetRequest.getUrlPath());
+        assertEquals("/pages/index.html", httpPostRequest.getUrlPath());
+    }
+
+    @Test
+    void getQueryParams()
+    {
+        Map<String, String> queryParams = httpGetRequest.getQueryParams();
+        assertEquals(3, queryParams.size());
     }
 }
